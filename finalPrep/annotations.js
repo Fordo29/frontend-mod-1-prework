4af19e9fd4e050303ad340b5fdf1980d526906a9
Christine R <90149529+Fordo29@users.noreply.github.com>
2021-09-21 18:08:07 -0600

    Update annotations.js

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -2,11 +2,12 @@
 // Use the double // to create a new comment
 
 //Build a Bear
-function buildABear(name, age, fur, clothes, specialPower) {
-  var greeting = `Hey partner! My name is ${name} - will you be my friend?!`;
-  var demographics = [name, age];
-  var powerSaying = "Did you know that I can " + specialPower + " ?";
-  var builtBear = {
+function buildABear(name, age, fur, clothes, specialPower) //buildABear is the class name and (items listed in the parenthesis are parameters.)
+{
+  var greeting = `Hey partner! My name is ${name} - will you be my friend?!`; //This is the greeting property that is a string that interpolates a name from another variable
+  var demographics = [name, age]; // dynamic properties name and age.
+  var powerSaying = "Did you know that I can " + specialPower + " ?"; //This is a property that strings together with concatenation a dynamic variable.
+  var builtBear = {  //This is a property of an object that is another object with properties.  This object lists the dynamic properties of the bear. 
     basicInfo: demographics,
     clothes: clothes,
     exterior: fur,
@@ -15,28 +16,46 @@ function buildABear(name, age, fur, clothes, specialPower) {
     isCuddly: true,
   };
 
-  return builtBear
+  return builtBear //This method will show the properies of the completed bear
 }
 
-buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares');
-buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in');
-
-
+buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares'); 
+//     basicInfo: name: Fluffy, age: 4 
+//     clothes: ['pants', 'jorts', 'tanktop'],
+//     exterior: brown,
+//     cost: 49.99,
+//     sayings: [`Hey partner! My name is Fluffy - will you be my friend?!`, "Did you know that I can give you nightmares?", "Goodnight my friend!"],
+//     isCuddly: true,
+      
+buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in'); 
+//     basicInfo: name: Sleepy, age: 2
+//     clothes: ['pajamas', 'sleeping cap'],
+//     exterior: purple,
+//     cost: 49.99,
+//     sayings: [`Hey partner! My name is Sleepy - will you be my friend?!`, "Did you know that I can sleeping cap?", "Goodnight my friend!"],
+//     isCuddly: true,
 
 //FizzBuzz
-function fizzBuzz(num1, num2, range) {
-  for (var i = 0; i <= range; i++) {
-    if (i % num1 === 0 && i % num2 === 0) {
-      console.log('fizzbuzz');
-    } else if (i % num1 === 0) {
-      console.log('fizz');
-    } else if (i % num2 === 0) {
-      console.log('buzz');
+function fizzBuzz(num1, num2, range) //fizzBuzz is the class name (items in the parathesis are parameters)
+{ 
+  for (var i = 0; i <= range; i++) //This lists out our loop - starts at 0 and goes to range listed in the argument and increments up by one.
+  {
+    if (i % num1 === 0 && i % num2 === 0) // Cycles through the loop following these if statements
+    {
+      console.log('fizzbuzz'); // If the conditions are met, the number will be replaced with this 'fizzbuzz'. 
+    } 
+    else if (i % num1 === 0) //If the condition is is not met above the loop continues here.
+    {
+      console.log('fizz');// if the condition is met here, the number will be replaced with 'fizz'
+    } 
+    else if (i % num2 === 0)//if the condition for the loop on a number is not met in the 2 previous conditions it continues here. 
+    {
+      console.log('buzz'); //if the condition is met here, the number will be replaced with the word 'buzz'.
     } else {
-      console.log(i);
+      console.log(i); //If none of the conditions are met while proceeding through the loop, the number will be displayed. 
     }
   }
 }
 
-fizzBuzz(3, 5, 100);
+fizzBuzz(3, 5, 100); // these are the object instances with the list of arguments that are passed into our function above. 
 fizzbuzz(5, 8, 400);
